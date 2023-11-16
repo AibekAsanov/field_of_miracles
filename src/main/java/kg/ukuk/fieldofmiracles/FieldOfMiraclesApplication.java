e90e24c976c863a0a4f402b7facc8efe884684f4
Aibek Asanov <aibek1382@gmail.com>
2023-11-16 09:13:18 +0600

    initial commit

---
+++ src/main/java/kg/ukuk/fieldofmiracles/FieldOfMiraclesApplication.java
@@ -0,0 +1,13 @@
+package kg.ukuk.fieldofmiracles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldOfMiraclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldOfMiraclesApplication.class, args);
+	}
+
+}
